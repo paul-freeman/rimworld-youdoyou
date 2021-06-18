da31126cd0968e577ca10ebb58bdbd6b168f8cb8
Paul Freeman <paul.freeman.cs@gmail.com>
2021-06-19 10:45:40 +1200

    beta 2 hotfix 1

--- YouDoYou_Settings.cs
+++ YouDoYou_Settings.cs
@@ -13,31 +13,31 @@ namespace YouDoYou
         {
             WorkTypeAdjustment = new Dictionary<string, float>();
             _scrollPosition = Vector2.zero;
-            _listingStandard = new Listing_Standard();
             _height = 500.0f;
-            _workTypes = DefDatabase<WorkTypeDef>.AllDefsListForReading;
         }
 
         public static void DoSettingsWindowContents(Rect inRect)
         {
-            Rect _view = new Rect(15.0f, 0, inRect.width - 30.0f, inRect.height);
+            var _view = new Rect(15.0f, 0, inRect.width - 30.0f, inRect.height);
+            var _listingStandard = new Listing_Standard();
+            var _workTypes = DefDatabase<WorkTypeDef>.AllDefsListForReading;
+            var _sliderValue = 0.0f;
+
             _view.height = _height;
             Widgets.BeginScrollView(inRect, ref _scrollPosition, _view);
             GUI.BeginGroup(_view);
-            _view.height = 9999;
+            _view.height = 9999.0f;
             _listingStandard.Begin(_view.ContractedBy(10.0f));
 
             _listingStandard.Label("YouDoYouWorkTypeAdjustments".Translate("YouDoYouPriorityColonyPolicy".TranslateSimple()));
             _listingStandard.GapLine(40.0f);
-            float _sliderValue;
+
+            // draw sliders for each work type
             foreach (WorkTypeDef workTypeDef in _workTypes)
             {
                 WorkTypeAdjustment.TryGetValue(workTypeDef.defName, out _sliderValue);
                 _listingStandard.Label(
-                    "YouDoYouWorkTypeAdjustment".Translate(
-                        workTypeDef.defName.Translate(),
-                        Mathf.RoundToInt(_sliderValue * 100.0f)
-                    ),
+                    "YouDoYouWorkTypeAdjustment".Translate(workTypeDef.defName, Mathf.RoundToInt(_sliderValue * 100.0f)),
                     tooltip: workTypeDef.description
                 );
                 WorkTypeAdjustment.SetOrAdd(
@@ -46,6 +46,8 @@ namespace YouDoYou
                 );
                 _listingStandard.Gap(18.0f);
             }
+
+            // draw button options
             if (_listingStandard.ButtonTextLabeled("YouDoYouResetSlidersLabel".TranslateSimple(), "YouDoYouResetSlidersButtonLabel".TranslateSimple()))
             {
                 foreach (WorkTypeDef workTypeDef in _workTypes)
@@ -71,7 +73,5 @@ namespace YouDoYou
 
         private static Vector2 _scrollPosition;
         private static float _height;
-        private static Listing_Standard _listingStandard;
-        private static List<WorkTypeDef> _workTypes;
     }
 }
